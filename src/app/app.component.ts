409f6eebf967628cc9c9bc5d8a9d7cde805cb1bd
madhava <dabaremadhava@gmail.com>
2019-08-18 13:53:16 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import * as JsBarcode from 'jsbarcode';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'barcode-angular';
+export class AppComponent implements AfterViewInit {
+
+  public doc: any;
+
+  @ViewChild('pdfViewerAutoLoad', {static: false}) pdfViewerAutoLoad;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  ngAfterViewInit(): void {
+
+    const barcodeoptons = {
+      format: 'CODE128',
+      // width: 100,
+      // height: 90,
+      displayValue: true
+    };
+    JsBarcode('#barcode', '1234', barcodeoptons);
+    const imgData = (document.getElementById('barcode') as HTMLCanvasElement).toDataURL('image/png', 1);
+
+    this.doc = new jsPDF('p', 'mm', [297, 210]);
+
+    // this.doc.setFontSize(40);
+    // this.doc.text(35, 25, 'Paranyan loves jsPDF');
+    this.doc.addImage(imgData, 'PNG', 0, 0, 10, 10);
+    this.doc.addImage(imgData, 'PNG', 11, 0, 10, 10);
+
+    this.pdfViewerAutoLoad.pdfSrc = this.doc.output('bloburl'); // pdfSrc can be Blob or Uint8Array
+    this.pdfViewerAutoLoad.refresh(); // Ask pdf viewer to load/refresh pdf
+  }
+
 }
+// npm install jsbarcode --save
+// npm install ng2-pdfjs-viewer --save
+// npm install jspdf --save
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PdfJsViewerModule} from 'ng2-pdfjs-viewer';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PdfJsViewerModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
